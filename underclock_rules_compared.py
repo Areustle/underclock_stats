faf461bf5532ff6f7f28eb6c2aca8c52e68c1732
Alex Reustle <areustledev@gmail.com>
2023-05-01 10:03:34 -0400

    Add classic random encounter rule

--- underclock_rules_compared.py
+++ underclock_rules_compared.py
@@ -29,7 +29,7 @@ def underclock(rule, n, d=6, tries=20):
 
 def original_rule(rolls, clock, ticks, omens, run, d, x=3):
     """
-    Original underclock rule.
+    Goblinpunch underclock rule.
     - Encounter triggered when clock goes negative.
     - Ticks repeat on a Max roll (6 for d6)
     - Clock resets to x(=3) at exactly zero
@@ -59,6 +59,17 @@ def alternative_rule(rolls, clock, ticks, omens, run, d, x):
     return clock, ticks, omens, run
 
 
+def classic_random_encounter(rolls, clock, ticks, omens, run, *args):
+    r"""
+    A classic random encounter system where every turn the random encounter die
+    is rolled. On a 1 the encounter is triggered. On a 2 an omen is triggered.
+    """
+    run[rolls == 1] = False
+    ticks[run] += 1
+    omens[run & (rolls == 2)] = True
+    return clock, ticks, omens, run
+
+
 def plot_ticks(ax, bins, counts, color, label):
     """
     Histogram plot each of the rule tick results on provided ax.
@@ -75,14 +86,15 @@ def plot_ticks(ax, bins, counts, color, label):
 
 if __name__ == "__main__":
     N = int(1e6)
-    d = 4
+    d = 6
+    classic = underclock(lambda *args: classic_random_encounter(*args), N, d)
     orig = underclock(lambda *args: original_rule(*args, x=3), N, d)
     a4 = underclock(lambda *args: alternative_rule(*args, x=4), N, d)
     a3 = underclock(lambda *args: alternative_rule(*args, x=3), N, d)
     a2 = underclock(lambda *args: alternative_rule(*args, x=2), N, d)
 
     # ::::::::::::::::::: Plots :::::::::::::::::::
-    labels = ["Alt 2", "Alt 3", "Alt 4", "Original Rule"]
+    labels = ["Classic", "Alt 2", "Alt 3", "Alt 4", "Goblinpunch Rule"]
     fig, axs = plt.subplots(1, 2, figsize=(14, 7), layout="constrained")
     fig.suptitle(
         "Underclock Rule Comparisons\n" f" Dice = d{d}\n" f"{N:,} Samples Each"
@@ -93,15 +105,16 @@ if __name__ == "__main__":
     axs[0].set_xticks(np.arange(1, 21))
     axs[0].set_xlabel("Roll Count")
     axs[0].set_ylabel("Frequency (Percentage)")
-    plot_ticks(axs[0], *a2[0:2], "green", labels[0])
-    plot_ticks(axs[0], *a3[0:2], "orange", labels[1])
-    plot_ticks(axs[0], *a4[0:2], "red", labels[2])
-    plot_ticks(axs[0], *orig[0:2], "blue", labels[3])
+    plot_ticks(axs[0], *classic[0:2], "lightgray", labels[0])
+    plot_ticks(axs[0], *a2[0:2], "green", labels[1])
+    plot_ticks(axs[0], *a3[0:2], "orange", labels[2])
+    plot_ticks(axs[0], *a4[0:2], "red", labels[3])
+    plot_ticks(axs[0], *orig[0:2], "blue", labels[4])
     axs[0].legend()
 
     # Encounter Foreshadowing plot.
     axs[1].set_title("Encounter Foreshadowing")
-    omens = np.stack((a2[2], a3[2], a4[2], orig[2]), 1)
+    omens = np.stack((classic[2], a2[2], a3[2], a4[2], orig[2]), 1)
     x = np.arange(len(labels))
     multiplier = 0
 
